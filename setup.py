13472bfe2eefd4c16b1e5e04d1dc076e960f4512
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-09-30 11:01:11 -0400

    Update setup.py
    Realign setup.py with the version used in
    cisagov/skeleton-python-library now that we no longer need to support
    Python 2.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-This is the setup module for cyhy-feeds.
+This is the setup module for the cyhy-feeds project.
 
 Based on:
 
@@ -9,9 +9,9 @@ Based on:
 """
 
 # Standard Python Libraries
+import codecs
 from glob import glob
-import io
-from os.path import basename, splitext
+from os.path import abspath, basename, dirname, join, splitext
 
 # Third-Party Libraries
 from setuptools import find_packages, setup
@@ -19,35 +19,48 @@ from setuptools import find_packages, setup
 
 def readme():
     """Read in and return the contents of the project's README.md file."""
-    # Python 3
-    try:
-        with open("README.md", encoding="utf-8") as f:
-            return f.read()
-    # Python 2 fallback
-    except TypeError:
-        with io.open("README.md", encoding="utf-8") as f:
-            return f.read()
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
 
 
-def package_vars(version_file):
-    """Read in and return the variables defined by the version_file."""
-    pkg_vars = {}
-    with open(version_file) as f:
-        exec(f.read(), pkg_vars)  # nosec
-    return pkg_vars
+# Below two methods were pulled from:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+def read(rel_path):
+    """Open a file for reading from a given relative path."""
+    here = abspath(dirname(__file__))
+    with codecs.open(join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(version_file):
+    """Extract a version number from the given file path."""
+    for line in read(version_file).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="cyhy-feeds",
-    version=package_vars("aws_jobs/_version.py")["__version__"],
-    author="Cyber and Infrastructure Security Agency",
-    author_email="ncats@hq.dhs.gov",
-    url="https://www.us-cert.gov/resources/ncats",
-    download_url="https://github.com/cisagov/cyhy-feeds",
-    license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    # Versions should comply with PEP440
+    version=get_version("aws_jobs/_version.py"),
     description="Data feed components for Cyber Hygiene",
     long_description=readme(),
     long_description_content_type="text/markdown",
+    # Landing page for CISA's cybersecurity mission
+    url="https://www.cisa.gov/cybersecurity",
+    # Additional URLs for this project per
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#project-urls
+    project_urls={
+        "Source": "https://github.com/cisagov/skeleton-python-library",
+        "Tracker": "https://github.com/cisagov/skeleton-python-library/issues",
+    },
+    # Author details
+    author="Cybersecurity and Infrastructure Security Agency",
+    author_email="github@cisa.dhs.gov",
+    license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -60,13 +73,14 @@ setup(
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
+    python_requires=">=3.6",
     # What does your project relate to?
     keywords="cyhy",
     packages=find_packages(where="aws_jobs"),
@@ -84,6 +98,7 @@ setup(
         "pytz",
         "requests >= 2.18.4",
         "requests-aws4auth >= 0.9",
+        "setuptools >= 24.2.0",
     ],
     extras_require={
         "test": [
